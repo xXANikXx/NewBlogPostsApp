85fd0e7fe759b5d883e4ca97fa7f08a27f5eb08e
ANik <annikita.and@gmail.com>
2025-10-25 02:46:06 +0500

    lesson 6

--- src/auth/authorization/router/http-handler/authHandler.ts
+++ src/auth/authorization/router/http-handler/authHandler.ts
@@ -5,31 +5,25 @@ import {LoginRequestPayload} from "../request-payload/auth-request-payload";
 import {matchedData} from "express-validator";
 import {errorHandler} from "../../../../core/errors/errors.handler";
 import {ResultStatus} from "../../../../common/result/resultCode";
+import {
+    resultCodeToHttpException
+} from "../../../../common/result/resultCodeToHttpException";
 
 export async function loginHandler(req: Request, res: Response) {
     try {
         const data = matchedData(req) as LoginRequestPayload;
 
-        // 1. Получаем объект Result, содержащий { status: Success, data: { accessToken: '...' } }
         const result = await authService.loginUser(data.loginOrEmail, data.password);
 
-        // 2. Обработка неуспешного входа (Wrong credentials, Unauthorized)
         if (result.status !== ResultStatus.Success) {
-            return res.status(HttpStatus.Unauthorized).send({
-                errorsMessages: [
-                    { message: 'Unauthorized', field: 'loginOrEmail' },
-                ],
-            });
+            return res.status(resultCodeToHttpException(result.status)).send(result.extensions);
         }
-        // 3. Извлечение токена (строка)
         const accessToken = result.data!.accessToken;
 
-        // 4. Отправка ответа (200 OK с телом)
-        // Swagger: status 200; content: { "accessToken": "string" }
-        return res.status(HttpStatus.Ok).send({ accessToken });
+
+        return res.status(HttpStatus.Ok).send({ accessToken: result.data!.accessToken });
 
     } catch (e: unknown) {
-        // Ловит необработанные исключения (такие как сбой при res.send)
         errorHandler(e, res);
     }
 }
