389849d287452d536705a1058a654b1e92affce2
ANik <annikita.and@gmail.com>
2025-10-24 19:50:49 +0500

    lesson 6

--- src/auth/adapters/middlewares/access.token.guard.ts
+++ src/auth/adapters/middlewares/access.token.guard.ts
@@ -5,12 +5,23 @@ import {HttpStatus} from "../../../core/typesAny/http-statuses";
 
 export const accessTokenGuard = async (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers.authorization;
+    console.log('Authorization header:', authHeader); // 👈 добавь
+
+
     if (!authHeader) return res.sendStatus(HttpStatus.Unauthorized);
 
-    const [authType, token] = authHeader.split(' ');
+    console.log('Raw authHeader:', JSON.stringify(authHeader));
+
+    const [authType, token] = authHeader.trim().split(' ');
+    console.log('AuthType:', authType, 'Token:', token); // 👈 добавь
+
+
     if (authType !== 'Bearer' || !token) return res.sendStatus(HttpStatus.Unauthorized);
 
     const payload = await jwtService.verifyToken(token);
+    console.log('Decoded payload:', payload); // 👈 добавь
+
+
     if (!payload) return res.sendStatus(HttpStatus.Unauthorized);
 
     const { userId, userLogin } = payload;
