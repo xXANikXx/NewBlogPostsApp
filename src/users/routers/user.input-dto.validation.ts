529276e9edbc7aaa21f4301aa9e2d08acde9a25c
ANik <annikita.and@gmail.com>
2025-10-31 18:55:25 +0500

    lesson7

--- src/users/routers/user.input-dto.validation.ts
+++ src/users/routers/user.input-dto.validation.ts
@@ -3,8 +3,7 @@ import { body } from "express-validator"
 // Валидация логина
 const loginValidation = body('login')
     .notEmpty().withMessage('Login is required')
-    .isString()
-    .withMessage('Login must be a string')
+    .isString().withMessage('Login must be a string')
     .trim()
     .isLength({ min: 3, max: 10 })
     .withMessage('Login length must be between 3 and 10 characters')
@@ -13,20 +12,16 @@ const loginValidation = body('login')
 
 // Валидация пароля
 const passwordValidation = body('password')
-    .notEmpty().withMessage('Login is required')
-    .isString()
-    .withMessage('Password must be a string')
+    .notEmpty().withMessage('Password is required')
+    .isString().withMessage('Password must be a string')
     .trim()
-    .isLength({ min: 6, max: 20 })
-    .withMessage('Password length must be between 6 and 20 characters')
-
+    .isLength({ min: 6, max: 20 }).withMessage('Password length must be between 6 and 20 characters')
 // Валидация email
 const emailValidation = body('email')
-    .notEmpty().withMessage('Login is required')
-    .isString()
-    .withMessage('Email must be a string')
+    .notEmpty().withMessage('Email is required')
+    .isString().withMessage('Email must be a string')
     .trim()
-    .matches(/^[\w.-]+@([\w-]+\.)+[\w-]{2,4}$/)
+    .matches(/^[\w.+-]+@([\w-]+\.)+[\w-]{2,4}$/)
     .withMessage('Email must be a valid email address')
 
 // Экспортируем массив валидаторов
